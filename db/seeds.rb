4b60e2abc327ff4bac8bf54f3bc5708bc5e993dc
Nathan Babalogbon <nathanbabalogbon@gmail.com>
2023-06-07 11:53:28 +0100

    New seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,38 @@ require 'faker'
 
 puts "Cleaning database..."
 Gear.destroy_all
-# User.destroy_all
+User.destroy_all
 
 puts "Creating users..."
 puts "Gear..."
 
-
-
-5.times do
+50.times do
   user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.cell_phone,
     email: Faker::Internet.email,
     password: Faker::Internet.password
   )
   user.save!
+  rand(25).times do
+    gear = Gear.new(
+      model: Faker::Device.model_name,
+      brand: Faker::Device.manufacturer,
+      user_id: User.last.id,
+      rate_per_day: Faker::Number.decimal(l_digits: 2)
+    )
+    gear.save!
+  end
 end
-5.times do
-  gear = Gear.new(
-    model: Faker::Device.model_name,
-    brand: Faker::Device.manufacturer,
-    user_id: User.all.sample.id,
-    rate_per_day: Faker::Number.decimal(l_digits: 2)
-  )
-  gear.save!
-end
+
+# 200.times do
+#   gear = Gear.new(
+#     model: Faker::Device.model_name,
+#     brand: Faker::Device.manufacturer,
+#     user_id: User.all.sample.id,
+#     rate_per_day: Faker::Number.decimal(l_digits: 2)
+#   )
+#   gear.save!
+# end
